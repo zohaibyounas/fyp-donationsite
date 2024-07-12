0db14329cef9536dcf04445ad01b1642c72913da
OsamaBangash8840 <osama.khan8840@gmail.com>
2024-07-12 10:08:13 -0700

    .env and db.js modified

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`mongodb+srv://osamakhan8840:osama8840@cluster0.mp47cxf.mongodb.net/donation`, {
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.mp47cxf.mongodb.net/donation`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
